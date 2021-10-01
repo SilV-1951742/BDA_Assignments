29a05aa11f54574c452fd830edab4263718289d9
Sil Vaes <sil.g.vaes@gmail.com>
2021-10-01 21:28:50 +0200

    Custom parser to avoid special characters.

--- a1/main.py
+++ a1/main.py
@@ -2,14 +2,33 @@ from functools import partial
 import re
 import xml.etree.ElementTree as ET
 from html import unescape
+import sys
+import argparse
+import os
 
 
 entry_regex = re.compile(
     r"<(article|book|phdthesis|www|incollection|proceedings|inproceedings)[\s\S]*?<(\/article|\/book|\/phdthesis|\/www|\/incollection|\/proceedings|\/inproceedings)>"
 )
 
+author_regex = re.compile(r"<author>([\s\S]*?)<\/author>")
+title_regex = re.compile(r"<title>([\s\S]*?)<\/title>")
 
-def entry_string(filename, chunk_size):
+arg_parser = argparse.ArgumentParser(description="BDA opdr1.")
+
+arg_parser.add_argument("dataset", type=str, help="Dataset source file.")
+
+arg_parser.add_argument("chunksize", type=int, help="Chunksize in megabytes.")
+
+arg_parser.add_argument(
+    "--make_testfile",
+    action="store_true",
+    dest="testfile",
+    help="Create a test dataset of 10000 entries.",
+)
+
+
+def entry_string(filename: str, chunk_size: int):
     with open(filename, "r") as f:
         previous = ""
         start_prev = 0
@@ -23,18 +42,52 @@ def entry_string(filename, chunk_size):
             previous = data[start_prev:]
 
 
-def entry_tree(xml_string):
+def entry_tree(xml_string: str):
     return ET.fromstring(xml_string.replace("&", ""))
 
 
-def main():
-    gen_entry_string = entry_string("dblp.xml", 1 * 1024 * 1024)
+def create_author_set(xml_string: str) -> frozenset:
+    author_set = set()
+    for author in re.finditer(author_regex, xml_string):
+        author_set.add(author.group(1))
+    return frozenset(author_set)
 
-    for entry in gen_entry_string:
-        tmp_entry = unescape(entry)
-        print(f"{tmp_entry}\r\n\r\n\r\n")
-        root = entry_tree(tmp_entry)
-        print(f"Root tag {root.tag}, attrib {root.attrib}")
+
+def create_testfile(dataset: str, chunksize: int):
+    try:
+        os.remove("testfile.xml")
+    except OSError:
+        pass
+
+    gen_entry_string = entry_string(dataset, chunksize * 1024 * 1024)
+    author_set_list = []
+
+    with open("testfile.xml", "a") as f:
+        for _ in range(10000):
+            tmp_entry = unescape(next(gen_entry_string))
+            author_set_list.append(create_author_set(tmp_entry))
+            f.write(tmp_entry)
+
+
+def main():
+    args = arg_parser.parse_args()
+
+    if args.testfile:
+        create_testfile(args.dataset, args.chunksize)
+        exit()
+
+    author_set_list = []
+
+    try:
+        gen_entry_string = entry_string(args.dataset, args.chunksize * 1024 * 1024)
+        for entry in gen_entry_string:
+            tmp_entry = unescape(entry)
+            author_set_list.append(create_author_set(tmp_entry))
+    except FileNotFoundError:
+        print(f"Could not find file {args.dataset}")
+
+    for author in author_set_list:
+        print(author)
 
 
 if __name__ == "__main__":
