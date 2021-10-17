7e54ba2e245467af0b3a098e6f62f47d90bcd79e
SilV-1951742 <sil.vaes@student.uhasselt.be>
2021-10-17 20:39:16 +0200

    Added named tuple for year and title.

--- a2/main.py
+++ a2/main.py
@@ -2,14 +2,12 @@ from functools import partial
 import re
 from html import unescape
 import argparse
-import itertools
-from typing import Final
 from sklearn import cluster
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-import pandas as pd
 from nltk.stem import SnowballStemmer
 import matplotlib.pyplot as plt
+from typing import NamedTuple
 
 
 arg_parser = argparse.ArgumentParser(description="BDA opdr1.")
@@ -25,7 +23,11 @@ arg_parser.add_argument(
     help="Create a test dataset of 10000 entries.",
 )
 
-class title_generator:
+class year_title_tuple(NamedTuple):
+    year: int
+    title: str
+
+class year_title_generator:
     def __init__(self, filename: str, chunk_size: int):
         self.filename = filename
         self.chunk_size = chunk_size
@@ -33,6 +35,7 @@ class title_generator:
                 r"<(article|book|phdthesis|www|incollection|proceedings|inproceedings)[\s\S]*?<(\/article|\/book|\/phdthesis|\/www|\/incollection|\/proceedings|\/inproceedings)>"
 )
         self.title_regex = re.compile(r"<title>([\s\S]*?)<\/title>")
+        self.year_regex = re.compile(r"<year>([\s\S]*?)<\/year>")
 
     def entry_string_generator(self):
         previous = ""
@@ -47,12 +50,14 @@ class title_generator:
                 yield result.group(0)
             previous = data[start_prev:]
 
-    def create_title_string(self, xml_string: str) -> str:
-        return self.title_regex.search(xml_string).group(1)
+    def create_year_title_tuple(self, xml_string: str) -> year_title_tuple:
+        year: int = int(self.year_regex.search(xml_string).group(1))
+        title: str = self.title_regex.search(xml_string).group(1)
+        return year_title_tuple(year, title)
 
     def title_iterator(self):
         for entry in self.entry_string_generator():
-            yield self.create_title_string(unescape(entry))
+            yield self.create_year_title_tuple(unescape(entry))
 
     def __enter__(self):
         self.fp = open(self.filename, "r")
@@ -72,15 +77,16 @@ def stemmer_tokenizer(input_string: str):
 def main():
     args = arg_parser.parse_args()
 
-    title_collection = []
-    with title_generator(args.dataset, args.chunksize) as titles:
+    year_title_collection = []
+    with year_title_generator(args.dataset, args.chunksize) as titles:
         for title in titles.title_iterator():
-            title_collection.append(title)
+            print(title)
+            year_title_collection.append(title)
 
     print("Titles collected")
     
     min_hasher = TfidfVectorizer(norm='l1', use_idf=False, stop_words='english')
-    titles_hashed = min_hasher.fit_transform(title_collection)
+    titles_hashed = min_hasher.fit_transform([year_title.title for year_title in year_title_collection])
 
     print("Hashed")
     
