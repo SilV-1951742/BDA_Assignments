30a5d7e4f6aed40420f186baac49c10180358f08
SilV-1951742 <sil.vaes@student.uhasselt.be>
2021-10-20 21:37:22 +0200

    Problem with creating entries, silent regex crash.

--- a2/main.py
+++ a2/main.py
@@ -7,7 +7,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem import SnowballStemmer
 import matplotlib.pyplot as plt
-from typing import NamedTuple
+from typing import NamedTuple, Final
+import os
 
 
 arg_parser = argparse.ArgumentParser(description="BDA opdr1.")
@@ -23,6 +24,11 @@ arg_parser.add_argument(
     help="Create a test dataset of 10000 entries.",
 )
 
+DATA_MINING_KEYS: Final = ["journals/sigkdd", "conf/pkdd", "conf/icdm", "conf/sdm"]
+DB_KEYS: Final = ["journals/sigmod", "journals/vldb", "conf/edbt", "conf/icde"]
+
+g_entry = ""
+
 class year_title_tuple(NamedTuple):
     year: int
     title: str
@@ -30,19 +36,20 @@ class year_title_tuple(NamedTuple):
 class year_title_generator:
     def __init__(self, filename: str, chunk_size: int):
         self.filename = filename
+        print(f"Filesize: {os.path.getsize(filename)//(1024*1024)} MB")
         self.chunk_size = chunk_size
         self.entry_regex = re.compile(
-                r"<(article|book|phdthesis|www|incollection|proceedings|inproceedings)[\s\S]*?<(\/article|\/book|\/phdthesis|\/www|\/incollection|\/proceedings|\/inproceedings)>"
+            r"(<article|<book|<phdthesis|<www|<incollection|<proceedings|<inproceedings)([\s\S]*?)(<\/article>|<\/book>|<\/phdthesis>|<\/www>|<\/incollection>|<\/proceedings>|<\/inproceedings>)"
 )
         self.title_regex = re.compile(r"<title>([\s\S]*?)<\/title>")
         self.year_regex = re.compile(r"<year>([\s\S]*?)<\/year>")
+        #self.key_regex = re.compile(r"key=\"([\s\S]*?)\"")
+        self.key_regex = re.compile(r"<url>db\/([\s\S]*?)<\/url>")
 
     def entry_string_generator(self):
         previous = ""
         start_prev = 0
         file_r = partial(self.fp.read, self.chunk_size)
-        
-
         for data in iter(file_r, ""):
             data = previous + data
             for result in re.finditer(self.entry_regex, data):
@@ -55,11 +62,25 @@ class year_title_generator:
         title: str = self.title_regex.search(xml_string).group(1)
         return year_title_tuple(year, title)
 
+    def get_key(self, xml_string: str) -> str:
+        key = self.key_regex.search(xml_string)
+        key_str = ""
+        if key != None:
+            key_str = key.group(1)
+            name_index = key_str.rfind("/")
+            return key_str[0:name_index]
+        return ""
+    
     def title_iterator(self):
+        print(DATA_MINING_KEYS)
+        print(DB_KEYS)
         for entry in self.entry_string_generator():
+            if self.get_key(entry) in DATA_MINING_KEYS:
+                print(self.get_key(entry))
             yield self.create_year_title_tuple(unescape(entry))
 
     def __enter__(self):
+        print(f"Opening {self.filename}")
         self.fp = open(self.filename, "r")
         return self
 
@@ -78,34 +99,38 @@ def main():
     args = arg_parser.parse_args()
 
     year_title_collection = []
+    last_title = ""
     with year_title_generator(args.dataset, args.chunksize) as titles:
         for title in titles.title_iterator():
             # print(title)
             year_title_collection.append(title)
+            last_title = year_title_collection[-1].title
 
     print("Titles collected")
+    print(f"Last title: {last_title}")
+    print(f"Entries in filtered titles: {len(year_title_collection)}")
     
     min_hasher = TfidfVectorizer(norm='l2', use_idf=True, stop_words='english', ngram_range=(1, 1))
     
     print("Hashed")
 
-    for y in range(1960, 2020, 10):
-        clusters: int = 10
-        print(f"Clusters in range of year {y} - {y+10}")
-        titles_hashed = min_hasher.fit_transform([year_title.title for year_title  in year_title_collection if
-                                                  (year_title.year >= y and year_title.year < (y + 10))])
+    # for y in range(1960, 2020, 10):
+    #     clusters: int = 10
+    #     print(f"Clusters in range of year {y} - {y+10}")
+    #     titles_hashed = min_hasher.fit_transform([year_title.title for year_title  in year_title_collection if
+    #                                               (year_title.year >= y and year_title.year < (y + 10))])
     
-        km = cluster.KMeans(n_clusters=clusters)
-        km.fit(titles_hashed)
+    #     km = cluster.KMeans(n_clusters=clusters)
+    #     km.fit(titles_hashed)
 
-        print(f"Top terms per cluster {y} - {y+10}:")
-        order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-        terms = min_hasher.get_feature_names()
-        for i in range(clusters):
-            top_five_words = [terms[ind] for ind in order_centroids[i, :5]]
-            print("Cluster {}: {}".format(i, ' '.join(top_five_words)))
-        print()
-        print()
+    #     print(f"Top terms per cluster {y} - {y+10}:")
+    #     order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+    #     terms = min_hasher.get_feature_names()
+    #     for i in range(clusters):
+    #         top_five_words = [terms[ind] for ind in order_centroids[i, :5]]
+    #         print("Cluster {}: {}".format(i, ' '.join(top_five_words)))
+    #     print()
+    #     print()
 
     # plt.scatter(titles_hashed[:,0],titles_hashed[:,1], c=km.labels_, cmap='rainbow')
     # plt.scatter(km.cluster_centers_[:,0] ,km.cluster_centers_[:,1], color='black')
